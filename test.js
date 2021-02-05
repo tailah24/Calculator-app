eae8e7e2e6650496acd4bb07d89558186aa5ccc9
Tailah Adebanwo <tailahuno@gmail.com>
2021-02-04 23:05:00 -0500

    first commit

---
+++ test.js
@@ -0,0 +1,154 @@
+// function that prints from 1 to n number
+// function that prints from a to n number
+// function that prints from n down to 1 number
+// function that prints even numbers from 0 to n number
+// function that prints even numbers from n to n number
+// function that prints odd numbers from 0 to n number
+// function that prints  odd numbers from n to n number
+
+// function that returns an array of 1 to n number
+// function that returns an array of a to n number
+// function that returns an array of n down to 1 number
+// function that returns an array of even numbers from 0 to n number
+// function that returns an array of even numbers from n to n number
+// function that returns an array of odd numbers from 0 to n number
+// function that returns an array of odd numbers from 0 to n number
+
+
+// function that prints from 1 to n number
+// function numberFrom1ToNth(n) {
+//     for (let i = 1; i <= n; i++) {
+//         console.log(i);
+//     }
+
+// }
+// numberFrom1ToNth(5)
+
+
+// function that prints from a to n number
+// function numberFromAToN(a, n) {
+//     for ( let i = a; i <= n; i++) {
+//         console.log(i)
+//     }
+// }
+
+// numberFromAToN(-5, 12)
+
+
+// function that prints from n down to 1 number
+// function numberFromNto1(n) {
+//     for ( let i = n; i > 0; i--) {
+//         console.log(i)
+//     }
+// }
+// numberFromNto1(-5)
+
+// function that prints even numbers from 0 to n number
+// function evenNumber(n) {
+//     for ( let i = 0; i <= n; i++) {
+//             console.log(i)
+//             i++
+        
+//     }
+// }
+// evenNumber(13)
+
+// function that prints even numbers from n to n number
+// function evenNumberFromAtoN(a, n) {
+//     for ( let i = a; i <= n; i++) {
+//         if (i % 2 === 0) {
+//             console.log(i)
+//         }
+//     }
+// }
+// evenNumberFromAtoN(9, 24)
+
+// function that prints odd numbers from 0 to n number
+// function oddNumbers(n) {
+//     for ( let i = 0; i <= n; i++) {
+//         if (i % 2 !== 0) {
+//             console.log(i)
+//         }
+//     }
+// }
+
+// oddNumbers(20)
+
+// function that prints  odd numbers from n to n number
+// function oddNumbersFromAtoN (a, n) {
+//     for ( let i = a; i <= n; i++) {
+//         if (i % 2 !== 0) {
+//             console.log(i)
+//         }
+//     }
+// }
+
+// oddNumbersFromAtoN( 5, 20)
+
+
+// function that returns an array of 1 to n number
+// function arrayFrom1ToNth (n) {
+//     let output = []
+//     for ( let i = 1; i <= n; i++) {
+//         output.push(i)
+//     }
+//     console.log(output)
+//     return output;
+// }
+
+// arrayFrom1ToNth(9)
+
+// function that returns an array of a to n number
+// function numberFromAToN (a, n) {
+//     let output = []
+//     for ( let i = a; i <= n; i++) {
+//         output.push(i)
+//     }
+//     console.log(output)
+//     return output;
+// }
+
+// numberFromAToN( 5, 19)
+
+
+// function that returns an array of n down to 1 number
+// function numberFromNto1 (n) {
+//     let output = []
+//     for (i = n; i > 0; i--) {
+//         output.push(i)
+//     }
+
+//     console.log(output)
+//     return output
+// }
+
+// numberFromNto1 (7)
+
+
+// function that returns an array of even numbers from 0 to n number
+// function evenNumber(n) {
+//     let output = []
+//     for ( let i = 0; i <= n; i++) {
+//         output.push(i)
+//         i++
+//     }
+
+//     console.log(output)
+//     return output
+// }
+// evenNumber(9)
+
+
+// // function that returns an array of even numbers from n to n number
+// function evenNumberFromAtoN(a, n) {
+//     let output = []
+//     for ( let i = a; i <= n; i++) {
+//         if (i % 2 === 0) {
+//             output.push(i)
+//         }
+
+//     }
+//     console.log(output)
+//     return output
+// }
+// evenNumberFromAtoN(2, 15) 
